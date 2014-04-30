ce264b6c1de5204706f821a6712677c4aadccd16
YiweiWang <vvxmaseve@gmail.com>
2014-04-30 11:03:58 +0100

    initial

---
+++ test/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import processing.core.PApplet;
+
+
+public class Test extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { test.Test.class.getName() });
+	}
+}
